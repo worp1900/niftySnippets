6cdd45bcbe433cdfde729ea17fd8b6c23dbac4c1
worp1900 <worp@mindworp.de>
2015-02-06 09:43:55 +0100

    Added a SQL Server Query

---
+++ sql/microsoftSqlServer/getRowCountForAllTablesInSQLServer.sql
@@ -0,0 +1,10 @@
+CREATE TABLE #counts
+(
+    table_name varchar(255),
+    row_count int
+)
+
+EXEC sp_MSForEachTable @command1='INSERT #counts (table_name, row_count) SELECT ''?'', COUNT(*) FROM ?'
+SELECT table_name, row_count FROM #counts ORDER BY table_name, row_count DESC
+
+SELECT SUM(row_count) AS total_row_count FROM #counts
