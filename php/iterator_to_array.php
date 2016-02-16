24db78ffe85d31a94a3db66a3150b6c8852bb8ef
shuessner <sven.huessner@netlight.com>
2016-02-16 14:37:30 +0100

    A simple method to read out the storage of an ArrayObject (especially useful when IDEs can't access the storage in a debugging environment)

---
+++ php/iterator_to_array.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Display the contents of an ArrayObject's storage making use of its
+ * implementation of the Iterator interface.
+ * ArrayObject : http://php.net/manual/de/class.arrayobject.php
+*/
+
+iterator_to_array(ArrayObject $arrayObject)
